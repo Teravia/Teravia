ecebee9d014730c4a89662c8841c2ef24d199b5b
Teravia <teravia.vip@gmail.com>
2026-08-01 21:00:20 +0700

    Update index.ts

--- configs/hunian/index.ts
+++ configs/hunian/index.ts
@@ -1,1 +1,27 @@
+// configs/hunian/index.ts
 
+import rumah from "./rumah";
+import apartemen from "./apartemen";
+import villa from "./villa";
+import cluster from "./cluster";
+import townhouse from "./townhouse";
+import kos from "./kos";
+import asrama from "./asrama";
+
+const hunian = {
+  id: "hunian",
+  nama: "Hunian",
+  label: "Hunian",
+
+  propertyTypes: [
+    rumah,
+    apartemen,
+    villa,
+    cluster,
+    townhouse,
+    kos,
+    asrama,
+  ],
+};
+
+export default hunian;
