3df70ef0d99ac8f4bf617b0afd2b17f2e024f050
Teravia <teravia.vip@gmail.com>
2026-08-01 21:26:02 +0700

    Update index.ts

--- configs/hunian/index.ts
+++ configs/hunian/index.ts
@@ -1,16 +1,7 @@
-// configs/hunian/index.ts
-
 import rumah from "./rumah";
 
-
 const hunian = {
-  id: "hunian",
-  nama: "Hunian",
-  label: "Hunian",
-
-  propertyTypes: [
-    rumah,
-  ],
+  Rumah: rumah,
 };
 
 export default hunian;
