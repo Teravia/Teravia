51724e3d487f714c5972007ece206cd47c946cd9
Teravia <teravia.vip@gmail.com>
2026-08-01 21:00:55 +0700

    Update index.ts

--- configs/index.ts
+++ configs/index.ts
@@ -1,1 +1,44 @@
+// configs/index.ts
 
+import hunian from "./hunian";
+import komersial from "./komersial";
+import industri from "./industri";
+import tanah from "./tanah";
+
+const propertyConfigs = {
+  categories: [
+    hunian,
+    komersial,
+    industri,
+    tanah,
+  ],
+
+  getCategory(categoryId: string) {
+    return this.categories.find(
+      (category) => category.id === categoryId
+    );
+  },
+
+  getPropertyType(categoryId: string, propertyTypeId: string) {
+    const category = this.getCategory(categoryId);
+
+    if (!category) return null;
+
+    return category.propertyTypes.find(
+      (property) => property.id === propertyTypeId
+    );
+  },
+
+  getSections(categoryId: string, propertyTypeId: string) {
+    const property = this.getPropertyType(
+      categoryId,
+      propertyTypeId
+    );
+
+    if (!property) return [];
+
+    return property.sections;
+  },
+};
+
+export default propertyConfigs;
