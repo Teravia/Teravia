02857e323e710d5bd9ada44a11f446472d4c7a99
Teravia <teravia.vip@gmail.com>
2026-08-01 21:26:43 +0700

    Refactor propertyConfigs to CONFIG export

--- configs/index.ts
+++ configs/index.ts
@@ -1,39 +1,7 @@
-// configs/index.ts
-
 import hunian from "./hunian";
 
-
-const propertyConfigs = {
-  categories: [
-    hunian,
-  ],
-
-  getCategory(categoryId: string) {
-    return this.categories.find(
-      (category) => category.id === categoryId
-    );
-  },
-
-  getPropertyType(categoryId: string, propertyTypeId: string) {
-    const category = this.getCategory(categoryId);
-
-    if (!category) return null;
-
-    return category.propertyTypes.find(
-      (property) => property.id === propertyTypeId
-    );
-  },
-
-  getSections(categoryId: string, propertyTypeId: string) {
-    const property = this.getPropertyType(
-      categoryId,
-      propertyTypeId
-    );
-
-    if (!property) return [];
-
-    return property.sections;
-  },
+export const CONFIG: Record<string, any> = {
+  ...hunian,
 };
 
-export default propertyConfigs;
+export default CONFIG;
