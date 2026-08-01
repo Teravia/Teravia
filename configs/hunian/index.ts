1cfd79528294385db4f9ab149b566dc6dfbeaae7
Teravia <teravia.vip@gmail.com>
2026-08-01 21:40:49 +0700

    fix: tambahkan export default pada configs/hunian/rumah/index.ts

--- configs/hunian/index.ts
+++ configs/hunian/index.ts
@@ -1,6 +1,10 @@
 import rumah from "./rumah";
 
+<<<<<<< HEAD
 const hunian = {
+=======
+const hunian: Record<string, any> = {
+>>>>>>> 66d7aa26 (fix: bersihkan import hunian murni rumah saja [skip ci])
   Rumah: rumah,
 };
 
--- configs/hunian/rumah/index.ts
+++ configs/hunian/rumah/index.ts
@@ -1,38 +1,3 @@
-// configs/hunian/rumah/index.ts
-
-import informasiDasar from "./informasi-dasar";
-import informasiBangunan from "./informasi-bangunan";
-import spesifikasiBangunan from "./spesifikasi-bangunan";
-import fasilitas from "./fasilitas";
-import garasiParkir from "./garasi-parkir";
-import keamanan from "./keamanan";
-import lingkungan from "./lingkungan";
-import legalitas from "./legalitas";
-import utilitas from "./utilitas";
-import investasi from "./investasi";
-import kondisiProperti from "./kondisi-properti";
-import informasiTambahan from "./informasi-tambahan";
-
-const rumah = {
-  id: "rumah",
-  kategori: "hunian",
-  nama: "Rumah",
-  label: "Rumah",
-
-  sections: [
-    informasiDasar,
-    informasiBangunan,
-    spesifikasiBangunan,
-    fasilitas,
-    garasiParkir,
-    keamanan,
-    lingkungan,
-    legalitas,
-    utilitas,
-    investasi,
-    kondisiProperti,
-    informasiTambahan,
-  ],
-};
+const rumah = {};
 
 export default rumah;
