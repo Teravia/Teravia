fd541ed85fdc5500e9e6d2cdabf1db7f5aa8ee8b
Teravia <teravia.vip@gmail.com>
2026-08-01 22:54:32 +0700

    fix: import semua modular spesifikasi rumah ke index.ts

--- configs/hunian/rumah/index.ts
+++ configs/hunian/rumah/index.ts
@@ -1,3 +1,23 @@
-const rumah = {};
+import fasilitas from "./fasilitas";
+import garasiParkir from "./garasi-parkir";
+import informasiBangunan from "./informasi-bangunan";
+import informasiDasar from "./informasi-dasar";
+import informasiTambahan from "./informasi-tambahan";
+import keamanan from "./keamanan";
+import legalitas from "./legalitas";
+import lingkungan from "./lingkungan";
+import spesifikasiBangunan from "./spesifikasi-bangunan";
+
+const rumah = [
+  ...(Array.isArray(informasiDasar) ? informasiDasar : [informasiDasar]),
+  ...(Array.isArray(informasiBangunan) ? informasiBangunan : [informasiBangunan]),
+  ...(Array.isArray(spesifikasiBangunan) ? spesifikasiBangunan : [spesifikasiBangunan]),
+  ...(Array.isArray(legalitas) ? legalitas : [legalitas]),
+  ...(Array.isArray(garasiParkir) ? garasiParkir : [garasiParkir]),
+  ...(Array.isArray(fasilitas) ? fasilitas : [fasilitas]),
+  ...(Array.isArray(keamanan) ? keamanan : [keamanan]),
+  ...(Array.isArray(lingkungan) ? lingkungan : [lingkungan]),
+  ...(Array.isArray(informasiTambahan) ? informasiTambahan : [informasiTambahan]),
+].filter(Boolean);
 
 export default rumah;
