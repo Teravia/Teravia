531aa8dbf279caed0713d921f5e882f3caacdf6a
Teravia <teravia.vip@gmail.com>
2026-08-02 01:59:12 +0700

    Import modules and create apartemen array

--- configs/hunian/apartemen/index.ts
+++ configs/hunian/apartemen/index.ts
@@ -1,1 +1,40 @@
+// configs/hunian/apartemen/index.ts
+
+import informasiDasar from "./informasi-dasar";
+import kondisiProperti from "./kondisi-properti";
+import informasiUnit from "./informasi-unit";
+import spesifikasiUnit from "./spesifikasi-unit";
+import legalitas from "./legalitas";
+import parkir from "./parkir";
+import fasilitasUnit from "./fasilitas-unit";
+import fasilitasGedung from "./fasilitas-gedung";
+import keamanan from "./keamanan";
+import aksesibilitas from "./aksesibilitas";
+import utilitas from "./utilitas";
+import biayaRutin from "./biaya-rutin";
+import investasi from "./investasi";
+import lingkungan from "./lingkungan";
+import media from "./media";
+import informasiTambahan from "./informasi-tambahan";
+
+const apartemen = [
+  ...(Array.isArray(informasiDasar) ? informasiDasar : [informasiDasar]),
+  ...(Array.isArray(kondisiProperti) ? kondisiProperti : [kondisiProperti]),
+  ...(Array.isArray(informasiUnit) ? informasiUnit : [informasiUnit]),
+  ...(Array.isArray(spesifikasiUnit) ? spesifikasiUnit : [spesifikasiUnit]),
+  ...(Array.isArray(legalitas) ? legalitas : [legalitas]),
+  ...(Array.isArray(parkir) ? parkir : [parkir]),
+  ...(Array.isArray(fasilitasUnit) ? fasilitasUnit : [fasilitasUnit]),
+  ...(Array.isArray(fasilitasGedung) ? fasilitasGedung : [fasilitasGedung]),
+  ...(Array.isArray(keamanan) ? keamanan : [keamanan]),
+  ...(Array.isArray(aksesibilitas) ? aksesibilitas : [aksesibilitas]),
+  ...(Array.isArray(utilitas) ? utilitas : [utilitas]),
+  ...(Array.isArray(biayaRutin) ? biayaRutin : [biayaRutin]),
+  ...(Array.isArray(investasi) ? investasi : [investasi]),
+  ...(Array.isArray(lingkungan) ? lingkungan : [lingkungan]),
+  ...(Array.isArray(media) ? media : [media]),
+  ...(Array.isArray(informasiTambahan) ? informasiTambahan : [informasiTambahan]),
+].filter(Boolean);
+
+export default apartemen;
 
