64f03b7c7e0833d2c12c0cde3bbb23a4b4baaba1
Teravia <teravia.vip@gmail.com>
2026-08-01 22:58:28 +0700

    fix: bersihkan conflict marker di configs/hunian/index.ts

--- configs/hunian/index.ts
+++ configs/hunian/index.ts
@@ -1,10 +1,6 @@
 import rumah from "./rumah";
 
-<<<<<<< HEAD
-const hunian = {
-=======
 const hunian: Record<string, any> = {
->>>>>>> 66d7aa26 (fix: bersihkan import hunian murni rumah saja [skip ci])
   Rumah: rumah,
 };
 
