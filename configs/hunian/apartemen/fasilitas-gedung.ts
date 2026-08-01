ea188df8665ee30982cd171486be28dc8a10709d
Teravia <teravia.vip@gmail.com>
2026-08-02 01:58:06 +0700

    Update fasilitas-gedung.ts

--- configs/hunian/apartemen/fasilitas-gedung.ts
+++ configs/hunian/apartemen/fasilitas-gedung.ts
@@ -1,1 +1,152 @@
+// configs/hunian/apartemen/fasilitas-gedung.ts
 
+const fasilitasGedung = {
+  id: "fasilitas-gedung",
+  title: "Fasilitas Gedung",
+  description: "Fasilitas bersama yang tersedia di gedung apartemen.",
+
+  fields: [
+    {
+      id: "swimming_pool",
+      label: "Kolam Renang",
+      type: "checkbox",
+      required: false,
+    },
+
+    {
+      id: "kids_pool",
+      label: "Kolam Anak",
+      type: "checkbox",
+      required: false,
+    },
+
+    {
+      id: "gym",
+      label: "Fitness Center / Gym",
+      type: "checkbox",
+      required: false,
+    },
+
+    {
+      id: "jogging_track",
+      label: "Jogging Track",
+      type: "checkbox",
+      required: false,
+    },
+
+    {
+      id: "sky_lounge",
+      label: "Sky Lounge / Rooftop",
+      type: "checkbox",
+      required: false,
+    },
+
+    {
+      id: "function_hall",
+      label: "Function Hall / Ballroom",
+      type: "checkbox",
+      required: false,
+    },
+
+    {
+      id: "lobby_lounge",
+      label: "Lobby Lounge",
+      type: "checkbox",
+      required: false,
+    },
+
+    {
+      id: "coworking_space",
+      label: "Coworking Space",
+      type: "checkbox",
+      required: false,
+    },
+
+    {
+      id: "mini_market",
+      label: "Minimarket",
+      type: "checkbox",
+      required: false,
+    },
+
+    {
+      id: "laundry_service",
+      label: "Laundry Service",
+      type: "checkbox",
+      required: false,
+    },
+
+    {
+      id: "playground",
+      label: "Playground",
+      type: "checkbox",
+      required: false,
+    },
+
+    {
+      id: "bbq_area",
+      label: "Area BBQ",
+      type: "checkbox",
+      required: false,
+    },
+
+    {
+      id: "sauna",
+      label: "Sauna",
+      type: "checkbox",
+      required: false,
+    },
+
+    {
+      id: "spa",
+      label: "Spa",
+      type: "checkbox",
+      required: false,
+    },
+
+    {
+      id: "multipurpose_court",
+      label: "Lapangan Serbaguna",
+      type: "checkbox",
+      required: false,
+    },
+
+    {
+      id: "garden",
+      label: "Taman Gedung",
+      type: "checkbox",
+      required: false,
+    },
+
+    {
+      id: "prayer_room",
+      label: "Musholla / Ruang Ibadah",
+      type: "checkbox",
+      required: false,
+    },
+
+    {
+      id: "daycare",
+      label: "Daycare",
+      type: "checkbox",
+      required: false,
+    },
+
+    {
+      id: "library",
+      label: "Perpustakaan",
+      type: "checkbox",
+      required: false,
+    },
+
+    {
+      id: "clinic",
+      label: "Klinik",
+      type: "checkbox",
+      required: false,
+    },
+  ],
+};
+
+export default fasilitasGedung;
+      
