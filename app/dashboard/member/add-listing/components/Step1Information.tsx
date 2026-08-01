659ed31299602fc2c38f432ca79b3ab5c417ffbc
Teravia <teravia.vip@gmail.com>
2026-08-01 23:11:45 +0700

    Update Step1Information.tsx

--- app/dashboard/member/add-listing/components/Step1Information.tsx
+++ app/dashboard/member/add-listing/components/Step1Information.tsx
@@ -211,7 +211,7 @@ export default function Step1Information({
               </h3>
 
               <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                {(section.fields || []).map((field, fieldIdx) => {
+                {section.fields?.map((field, fieldIdx) => {
                   const fieldKey = `s${sectionIdx}_f${fieldIdx}`;
                   return (
                     <DynamicFieldRenderer
