e5100389862f2fc8c5af8af4cedba2a92f7c062e
Teravia <teravia.vip@gmail.com>
2026-08-02 11:44:04 +0700

    Add cluster import to hunian configuration

--- configs/hunian/index.ts
+++ configs/hunian/index.ts
@@ -1,9 +1,11 @@
 import rumah from "./rumah";
 import apartemen from "./apartemen";
+import cluster from "./cluster";
 
 const hunian: Record<string, any> = {
   Rumah: rumah,
   Apartemen: apartemen,
+  Cluster: cluster,
 };
 
 export default hunian;
