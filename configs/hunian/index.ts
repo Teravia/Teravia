c3590473dd67bb5b18d267f34603eec58b51f531
Teravia <teravia.vip@gmail.com>
2026-08-03 14:09:50 +0700

    Update index.ts

--- configs/hunian/index.ts
+++ configs/hunian/index.ts
@@ -5,6 +5,7 @@ import kontrakan from "./kontrakan";
 import penthouse from "./penthouse";
 import rusun from "./rusun";
 import kost from "./kost";
+import villa from "./villa";
 
 const hunian: Record<string, any> = {
   Rumah: rumah,
@@ -14,6 +15,7 @@ const hunian: Record<string, any> = {
   Penthouse: penthouse,
   Rusun: rusun,
   Kost: kost,
+  Villa: villa,
 };
 
 export default hunian;
