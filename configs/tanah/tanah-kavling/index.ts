4dc079572a6ab085f08aaf1c688be026923886dc
Teravia <teravia.vip@gmail.com>
2026-08-04 16:23:16 +0700

    Import and combine data sources into kavling array
    Consolidate various information sources into a single 'kavling' array for better data management.

--- configs/tanah/tanah-kavling/index.ts
+++ configs/tanah/tanah-kavling/index.ts
@@ -1,1 +1,24 @@
+// configs/tanah/kavling/index.ts
+
+import informasiDasar from "./informasi-dasar";
+import informasiKavling from "./informasi-kavling";
+import spesifikasiTanah from "./spesifikasi-tanah";
+import legalitas from "./legalitas";
+import infrastrukturAkses from "./infrastruktur-akses";
+import potensiInvestasi from "./potensi-investasi";
+import lingkungan from "./lingkungan";
+import informasiTambahan from "./informasi-tambahan";
+
+const kavling = [
+  ...(Array.isArray(informasiDasar) ? informasiDasar : [informasiDasar]),
+  ...(Array.isArray(informasiKavling) ? informasiKavling : [informasiKavling]),
+  ...(Array.isArray(spesifikasiTanah) ? spesifikasiTanah : [spesifikasiTanah]),
+  ...(Array.isArray(legalitas) ? legalitas : [legalitas]),
+  ...(Array.isArray(infrastrukturAkses) ? infrastrukturAkses : [infrastrukturAkses]),
+  ...(Array.isArray(potensiInvestasi) ? potensiInvestasi : [potensiInvestasi]),
+  ...(Array.isArray(lingkungan) ? lingkungan : [lingkungan]),
+  ...(Array.isArray(informasiTambahan) ? informasiTambahan : [informasiTambahan]),
+].filter(Boolean);
+
+export default kavling;
 
