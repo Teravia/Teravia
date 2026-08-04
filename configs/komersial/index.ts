6c30f44bfe06945c0d41627f4df30e58c13f60e6
Teravia <teravia.vip@gmail.com>
2026-08-04 09:39:59 +0700

    Update index.ts

--- configs/komersial/index.ts
+++ configs/komersial/index.ts
@@ -2,10 +2,12 @@
 
 import rukoRukan from "./ruko-rukan";
 import officeSpace from "./office-space";
+import tokoKios from "./toko-kios";
 
 const komersial: Record<string, any> = {
   "Ruko/Rukan": rukoRukan,
   "Office Space": officeSpace,
+  "Toko/Kios": tokoKios,
 };
 
 export default komersial;
