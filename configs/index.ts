5e64352ea94943981769840d90d5e70eaec123b0
Teravia <teravia.vip@gmail.com>
2026-08-04 16:24:17 +0700

    Update index.ts

--- configs/index.ts
+++ configs/index.ts
@@ -1,9 +1,11 @@
 import hunian from "./hunian";
 import komersial from "./komersial";
+import tanah from "./tanah";
 
 export const CONFIG: Record<string, any> = {
   Hunian: hunian,
   Komersial: komersial,
+  Tanah: tanah,
 };
 
 export default CONFIG;
