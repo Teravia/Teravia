b19758a952120a653cc7c9a1ead0791e93cd4299
Teravia <teravia.vip@gmail.com>
2026-08-05 22:00:26 +0700

    Add gedungPerkantoran to komersial record

--- configs/komersial/index.ts
+++ configs/komersial/index.ts
@@ -3,11 +3,13 @@
 import rukoRukan from "./ruko-rukan";
 import officeSpace from "./office-space";
 import tokoKios from "./toko-kios";
+import gedungPerkantoran from "./gedung-perkantoran";
 
 const komersial: Record<string, any> = {
   "Ruko/Rukan": rukoRukan,
   "Office Space": officeSpace,
   "Toko/Kios": tokoKios,
+  "Gedung Perkantoran": gedungPerkantoran,
 };
 
 export default komersial;
