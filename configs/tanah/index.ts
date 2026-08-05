52f1d9536bb3f7edefe3776c74639a3efe2eedfa
Teravia <teravia.vip@gmail.com>
2026-08-05 09:22:24 +0700

    Update index.ts

--- configs/tanah/index.ts
+++ configs/tanah/index.ts
@@ -1,10 +1,11 @@
 // configs/tanah/index.ts
 
 import kavling from "./kavling";
+import sawah from "./sawah";
 
 const tanah: Record<string, any> = {
   Kavling: kavling,
+  Sawah: sawah,
 };
 
 export default tanah;
-
